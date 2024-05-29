fdb52cefcd164d0d7d6fe6d2e858ab43daa0cf82
elias <defreitase@bzz.ch>
2024-05-29 15:54:16 +0200

    DB_V1 DONE

---
+++ DB/setup.sql
@@ -0,0 +1,69 @@
+DROP DATABASE IF EXISTS bFriend_DB;
+CREATE DATABASE bFriend_DB;
+USE bFriend_DB;
+
+CREATE TABLE Mode(
+    Mode_ID INT PRIMARY KEY AUTO_INCREMENT,
+    `Definition` VARCHAR(255)
+);
+
+CREATE TABLE Interests(
+    Interests_ID INT PRIMARY KEY AUTO_INCREMENT,
+    `Name` VARCHAR(255),
+    `Description` VARCHAR(255)
+);
+
+CREATE TABLE Card_Info(
+    Card_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Card_Holder VARCHAR(255),
+    Card_Number VARCHAR(255), 
+    Security_Code VARCHAR(255),
+    Card_ExpiryDate VARCHAR(255)
+);
+
+CREATE TABLE Subscription(
+    Subscription_ID INT PRIMARY KEY AUTO_INCREMENT,
+    `Description` VARCHAR(255),
+    Active BOOLEAN,
+    Sub_ExpiryDate DATE,
+    Sub_ActivationDate DATE
+);
+
+CREATE TABLE Message(
+    Message_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Sender_ID INT,
+    Receiver_ID INT,
+    Content VARCHAR(65536),
+    FOREIGN KEY (Sender_ID) REFERENCES Account(Account_ID),
+    FOREIGN KEY (Receiver_ID) REFERENCES Account(Account_ID)
+);
+
+CREATE TABLE Account(
+    Account_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Username VARCHAR(255),
+    `Name` VARCHAR(255),
+    Lastname VARCHAR(255),
+    `Description` VARCHAR(255),
+    Subscription_ID INT,
+    `Password` VARCHAR(255),
+    Admin_Rights BOOLEAN,
+    Mode_ID INT,
+    FOREIGN KEY (Subscription_ID) REFERENCES Subscription(Subscription_ID),
+    FOREIGN KEY (Mode_ID) REFERENCES Mode(Mode_ID)
+);
+
+CREATE TABLE Account_Interests(
+    Account_Interests_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Account_ID INT,
+    Interests_ID INT,
+    FOREIGN KEY (Interests_ID) REFERENCES Interests(Interests_ID),
+    FOREIGN KEY (Account_ID) REFERENCES Account(Account_ID)
+);
+
+CREATE TABLE Account_Card_Info(
+    Account_Card_Info_ID INT PRIMARY KEY AUTO_INCREMENT,
+    Card_ID INT,
+    Account_ID INT,
+    FOREIGN KEY (Card_ID) REFERENCES Card_Info(Card_ID),
+    FOREIGN KEY (Account_ID) REFERENCES Account(Account_ID)
+);
