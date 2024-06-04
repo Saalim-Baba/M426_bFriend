f7f293ba8d5e459cad913b1af54784559392eef5
moccil-bzz <moccil@bzz.com>
2024-06-04 20:55:20 +0200

    main page oder so

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-
-
 export default function App() {
-  return (
-      <div className="p-6 max-w-sm bg-white rounded-xl shadow-md flex items-center space-x-4">
-          <div>
-              <div className="text-xl font-medium text-black">ChitChat</div>
-              <p className="text-gray-500">You have a new message!</p>
-          </div>
-      </div>
-  )
+    return (
+        <div className="flex items-center justify-center h-screen" >
+            <div className="flex flex-col w-full max-w-md mx-auto bg-white rounded-lg shadow-lg" style={{ height: '75%' }}>
+                <div>
+                    Bild
+                </div>
+                <div className="flex flex-col justify-end h-full">
+
+                    <div className="flex m-3 border">
+                        <div className="mx-3">
+                            <img
+                                alt="retard user"
+                                src="/11c7a56403bb2371acfa14a797b14571.webp"
+                                className="rounded-full w-12"
+                            />
+                        </div>
+                        <div>
+                            I like to bombastic the rizz out of people #brainrot #fortnitehater
+                        </div>
+                    </div>
+
+                    <div className="flex justify-between m-3 border">
+                        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-l">
+                            Reject
+                        </button>
+                        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-r">
+                            Accept
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
