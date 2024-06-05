d48de9885599b75fc4d3e22bef1860b14914b000
Hustini <ejdersalih02@gmail.com>
2024-06-05 12:16:08 +0200

    endpoint login is now working with DB

--- backend/app.js
+++ backend/app.js
@@ -87,16 +87,38 @@ app.get('/account', async (req, res) => {
     }
 });
 
+app.get('/login', async (req, res) => {
+    let username = req.query.username;
+    let password = req.query.password;
 
-app.get('/login', (req, res) => {
-    let username = req.query.username
-    let password = req.query.password
-    let customer = data["customers"].find((customer) => customer.username === username)
-    if (password === customer["password"]) {
-        return res.send(customer).status(200)
+    let conn;
+    let result;
+    try {
+        conn = await pool.getConnection();
+        result = await conn.query("SELECT * FROM account;");
+    } catch (err) {
+        console.error('Database query error:', err);
+        return res.status(500).send(err.message);
+    } finally {
+        if (conn) await conn.release();
     }
-    res.sendStatus(404)
-})
+
+    // Find the customer based on username
+    let customer = result.find((customer) => customer.Username === username);
+    console.log('Found customer:', customer);
+
+    if (customer) {
+        if (password === customer.Password) {
+            return res.status(200).send(customer);
+        } else {
+            return res.status(401).send('Incorrect password');
+        }
+    } else {
+        console.log(`Customer with username ${username} not found.`);
+        return res.sendStatus(404);
+    }
+});
+
 
 app.post('/register', (req, res) => {
     let newData = req.body;
