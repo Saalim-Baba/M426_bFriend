0ba3343baab9ea80ab1b8f0ee4cf09c482400804
moccil-bzz <moccil@bzz.com>
2024-06-05 11:49:57 +0200

    sign in changed

--- frontend/src/Components/SignIn.js
+++ frontend/src/Components/SignIn.js
@@ -8,10 +8,7 @@ function SignIn() {
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                     Sign in to your account
                 </h2>
-                <p className="mt-2 text-center text-sm text-gray-600 max-w">
-                    Or
-                    <Link to="/signup" className="font-medium text-blue-600 hover:text-blue-500">
-                        create an account
+                <p className="mt-2 text-center text-sm text-gray-600 max-w">Or <Link to="/signup" className="font-medium text-blue-600 hover:text-blue-500">create an account
                     </Link>
                 </p>
             </div>
@@ -64,42 +61,6 @@ function SignIn() {
                             </button>
                         </div>
                     </form>
-                    <div className="mt-6">
-                        <div className="relative">
-                            <div className="absolute inset-0 flex items-center">
-                                <div className="w-full border-t border-gray-300"></div>
-                            </div>
-                            <div className="relative flex justify-center text-sm">
-                                <span className="px-2 bg-white text-gray-500">
-                                    Or continue with
-                                </span>
-                            </div>
-                        </div>
-
-                        <div className="mt-6 grid grid-cols-3 gap-3">
-                            <div>
-                                <Link to="#"
-                                      className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                    <img className="h-5 w-5" src="https://www.svgrepo.com/show/512120/facebook-176.svg"
-                                         alt="Facebook" />
-                                </Link>
-                            </div>
-                            <div>
-                                <Link to="#"
-                                      className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                    <img className="h-5 w-5" src="https://www.svgrepo.com/show/513008/twitter-154.svg"
-                                         alt="Twitter" />
-                                </Link>
-                            </div>
-                            <div>
-                                <Link to="#"
-                                      className="w-full flex items-center justify-center px-8 py-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                                    <img className="h-6 w-6" src="https://www.svgrepo.com/show/506498/google.svg"
-                                         alt="Google" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
