0ed6ffc965cfc479a61afe077ea8c61719aef744
moccil-bzz <moccil@bzz.com>
2024-06-05 11:10:22 +0200

    button funktionalität

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Welcome User</span>
                 </Link>
                 <div className="flex items-center lg:order-2">
-                    <Link to="/download"
+                    <Link to="/message"
                           className="text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 sm:mr-2 lg:mr-0 dark:bg-purple-600 dark:hover:bg-purple-700 focus:outline-none dark:focus:ring-purple-800">Message</Link>
                     {/* Add more buttons or links here if necessary */}
                 </div>
